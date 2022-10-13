304cff586375ae6d1a923b30423330fc4a070331
Mr.Da <zhangda2@yonyou.com>
2022-10-13 14:51:53 +0800

    init ant-design-pro

---
+++ alTool/src/pages/tooltype/index.tsx
@@ -0,0 +1,89 @@
+import { PlusOutlined } from '@ant-design/icons';
+import { Button, Card, List, Typography } from 'antd';
+import { PageContainer } from '@ant-design/pro-layout';
+import { useRequest } from 'umi';
+import { queryFakeList } from './service';
+import type { CardListItemDataType } from './data.d';
+import styles from './style.less';
+
+const { Paragraph } = Typography;
+
+const CardList = () => {
+  const { data, loading } = useRequest(() => {
+    return queryFakeList({
+      count: 8,
+    });
+  });
+
+  const list = data?.list || [];
+
+  const content = (
+    <div className={styles.pageHeaderContent}>
+      <p>
+        工具类别新增、修改、删除处理。
+      </p>
+    </div>
+  );
+
+  const extraContent = (
+    <div className={styles.extraImg}>
+      <img
+        alt="这是一个分类"
+        src="https://gw.alipayobjects.com/zos/rmsportal/RzwpdLnhmvDJToTdfDPe.png"
+      />
+    </div>
+  );
+  const nullData: Partial<CardListItemDataType> = {};
+  return (
+    <PageContainer content={content} extraContent={extraContent}>
+      <div className={styles.cardList}>
+        <List<Partial<CardListItemDataType>>
+          rowKey="id"
+          loading={loading}
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 2,
+            md: 3,
+            lg: 3,
+            xl: 4,
+            xxl: 4,
+          }}
+          dataSource={[nullData, ...list]}
+          renderItem={(item) => {
+            if (item && item.id) {
+              return (
+                <List.Item key={item.id}>
+                  <Card
+                    hoverable
+                    className={styles.card}
+                    actions={[<a key="option1">修改</a>, <a key="option2">删除</a>]}
+                  >
+                    <Card.Meta
+                      avatar={<img alt="" className={styles.cardAvatar} src={item.avatar} />}
+                      title={<a>{item.title}</a>}
+                      description={
+                        <Paragraph className={styles.item} ellipsis={{ rows: 3 }}>
+                          {item.description}
+                        </Paragraph>
+                      }
+                    />
+                  </Card>
+                </List.Item>
+              );
+            }
+            return (
+              <List.Item>
+                <Button type="dashed" className={styles.newButton}>
+                  <PlusOutlined /> 新增分类
+                </Button>
+              </List.Item>
+            );
+          }}
+        />
+      </div>
+    </PageContainer>
+  );
+};
+
+export default CardList;
