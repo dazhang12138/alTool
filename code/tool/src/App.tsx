a1f7d357d4cfddc4b5f880c422d79333871eb016
Mr.Da <836160973da@gmail.com>
2022-10-20 14:03:49 +0800

    npm init tool

---
+++ code/tool/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ToolType from "./page/toolType";
+
+function App() {
+  return (
+    <div className="App">
+      <ToolType/>
+    </div>
+  );
+}
+
+export default App;
---
+++ code/tool/src/page/toolType.tsx
@@ -0,0 +1,10 @@
+import {FC} from "react";
+
+const ToolType:FC = (props) => {
+
+    return(
+        <span>123456</span>
+    );
+}
+
+export default ToolType;
