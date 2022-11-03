1238df822db5e3e98dac9d7e83eef871b63f36ae
Mr.Da <836160973da@gmail.com>
2022-11-03 14:41:15 +0800

    正则表达式计算器页面

--- code/tool/src/components/regularExpressionTool/index.tsx
+++ code/tool/src/components/regularExpressionTool/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import {Breadcrumb, Button, CheckboxGroup, Space, TextArea, Typography} from "@douyinfe/semi-ui";
+import React, {useState} from "react";
+import {Breadcrumb, Button, ButtonGroup, CheckboxGroup, Space, TextArea, Typography} from "@douyinfe/semi-ui";
 import {IconHome, IconPriceTag} from "@douyinfe/semi-icons";
 import {useNavigate} from "react-router-dom";
 import {checkboxGroupOptions, commomData} from "./data";
@@ -9,6 +9,14 @@ const { Title,Paragraph, Text } = Typography;
 export const AlRegularExpressionTool = () => {
 
     const navigate = useNavigate();
+    //正则全局设置
+    const [global,setGlobal] = useState([] as any[]);
+    //正则表达式
+    const [regular,setRegular] = useState('');
+    //处理文本
+    const [text,setText] = useState('');
+    //结果
+    const [result,setResult] = useState('');
 
     return(
       <div style={{margin: '20px 10px'}}>
@@ -18,19 +26,23 @@ export const AlRegularExpressionTool = () => {
               <Breadcrumb.Item>正则表达式工具</Breadcrumb.Item>
           </Breadcrumb>
           <Title heading={2} style={{ margin: '8px 0' }} >正则表达式工具</Title>
-          <Title heading={6} style={{ margin: '8px 0' }} >常用正则：</Title>
-          <Space>
-              {commomData.map((item => <Button theme='borderless' type='secondary' style={{ marginRight: 8 }}>{item.key}</Button>))}
+          <Space vertical style={{width:'100%'}} align={'start'}>
+              <Title heading={6} style={{ margin: '8px 0' }} >常用正则：</Title>
+              <ButtonGroup theme='borderless' type='secondary' size={'small'}>
+                  {commomData.map((item => <Button onClick={()=>{
+                      setRegular(item.dataRegex);
+                  }}>{item.key}</Button>))}
+              </ButtonGroup>
+              <CheckboxGroup options={checkboxGroupOptions} direction='horizontal' value={global} onChange={(value)=>setGlobal(value)}/>
+              <TextArea value={regular} onChange={(value) => setRegular(value)}/>
+              <Space>
+                  <Button>生成程序代码</Button>
+                  <Button>分享正则</Button>
+              </Space>
+              <TextArea value={text} onChange={(value) => setText(value)}/>
+              <Title heading={5} style={{ margin: '8px 0' }}>共找到 1 处匹配结果</Title>
+              <TextArea value={result} onChange={(value) => setResult(value)}/>
           </Space>
-          <CheckboxGroup options={checkboxGroupOptions} direction='horizontal' />
-          <TextArea />
-          <Space>
-              <Button>生成程序代码</Button>
-              <Button>分享正则</Button>
-          </Space>
-          <TextArea />
-          <Title heading={4} style={{ margin: '8px 0' }}>共找到 1 处匹配结果</Title>
-          <TextArea />
       </div>
     )
 }
